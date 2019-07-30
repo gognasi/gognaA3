6924a0299735a3c9dfea3c6c7e118d916169b346
gognasi <52754625+gognasi@users.noreply.github.com>
2019-07-29 21:30:19 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,7 @@
+import { Songs } from '../../app/songInfo';
+
+export const SONGINFO: Songs[] = 
+[{songName:'Old Town Road', artist:'Lil Nas X', genre:'Country', yearReleased:2019, picture:'../assets/images/pic1.jpg'  },
+{songName:'bad guy', artist:'Bille Ellish', genre:'Pop', yearReleased:2019, picture:'../assets/images/pic2.jpg'  },
+{songName:'I Dont Care', artist:'Justin Bieber', genre:'Pop', yearReleased:2019, picture:'../assets/images/pic3.jpg'  },
+{songName:'Sucker', artist:'Jonas Brothers', genre:'Pop', yearReleased:2019, picture:'../assets/images/pic4.jpg'  }]
---
+++ assets/data/simranInfo.ts
@@ -0,0 +1,5 @@
+import { Gognasi } from '../../app/gognasi';
+
+export const SIMRANINFO: Gognasi[] = 
+[{studentNumber: 991439731, name: 'Simran Gogna',
+loginName:'gognasi', campus: 'Davis', assignmentTitle: 'Assignment Three: Angular Intro'}]
